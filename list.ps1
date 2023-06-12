8bb92a76719b4700d4b8b67356d15eb21ea2afc7
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-06-12 22:59:55 +0530

    Create list.ps1

---
+++ list.ps1
@@ -0,0 +1,24 @@
+$policyAssignments = az policy assignment list --query "[?contains(displayName, 'BaringsCCP')].[displayName, description, enforcementMode, scope, excludedScopes, parametersEffectValue]" --output json | ConvertFrom-Json
+
+$formattedAssignments = if ($policyAssignments) {
+    $policyAssignments | ForEach-Object {
+        $_.displayName = $_.displayName ?? "N/A"
+        $_.description = $_.description ?? "N/A"
+        $_.scope = $_.scope ?? "N/A"
+        $_.enforcementMode = $_.enforcementMode ?? "N/A"
+        $_.excludedScopes = $_.excludedScopes ?? "N/A"
+        $_
+    }
+} else {
+    [PSCustomObject]@{
+        displayName = "N/A"
+        description = "N/A"
+        scope = "N/A"
+        enforcementMode = "N/A"
+        excludedScopes = "N/A"
+    }
+}
+
+$headers = "DisplayName", "Description", "Scope", "EnforcementMode", "ExcludedScopes"
+$table = $formattedAssignments | Format-Table -Property $headers -AutoSize | Out-String
+Write-Output $table
